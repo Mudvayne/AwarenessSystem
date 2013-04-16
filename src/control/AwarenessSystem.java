025236a6d202796df87663b07d3573cb3ffd83d0
Mudvayne <Mudvayne@129.187.118.144>
2013-04-16 12:21:55 +0200

    Grundgerüst

---
+++ src/control/AwarenessSystem.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mudvayne
+ */
+public class AwarenessSystem extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("CalendarView.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("FilterView.fxml"));
+        
+        stage.setScene(new Scene(root));
+        stage.setTitle("Team Awareness System");
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
