54e22a9a31d32413d0f1a016dce534546fe1a6a9
michi <>
2013-05-07 11:19:35 +0200

    Testing Events - Google Kalener API

--- src/test/GoogleAPItest.java
+++ src/test/GoogleAPItest.java
@@ -9,10 +9,13 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.calendar.Calendar;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Calendar;
 import com.google.api.services.calendar.model.CalendarList;
 import com.google.api.services.calendar.model.CalendarListEntry;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -31,6 +34,8 @@ public class GoogleAPItest {
     /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
+    private static final String SERVICE_ACCOUNT = "777633749381-sesph1uuf3un0mtpipob67qsc67krctv@developer.gserviceaccount.com";
+    
     public static void main(String[] args) {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -38,22 +43,35 @@ public class GoogleAPItest {
             // service account credential (uncomment setServiceAccountUser for domain-wide delegation)
             GoogleCredential credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT)
                 .setJsonFactory(JSON_FACTORY)
-                .setServiceAccountId("777633749381-sesph1uuf3un0mtpipob67qsc67krctv@developer.gserviceaccount.com")
+                .setServiceAccountId(SERVICE_ACCOUNT)
                 .setServiceAccountScopes(CalendarScopes.CALENDAR_READONLY)
                 .setServiceAccountPrivateKeyFromP12File(new File("key.p12"))
                 .build();
 
-            Calendar calendar = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("TeamAwarenessSystem/0.1").build();
+            com.google.api.services.calendar.Calendar client = new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("TeamAwarenessSystem/0.1").build();
+            
+            CalendarList feed = client.calendarList().list().execute();
 
-
-            CalendarList feed = calendar.calendarList().list().execute();
-
+            java.util.Calendar cal = java.util.Calendar.getInstance();
+            cal.set(java.util.Calendar.HOUR_OF_DAY, 0);
+            cal.set(java.util.Calendar.MINUTE, 0);
+            cal.set(java.util.Calendar.SECOND, 0);
+            cal.set(java.util.Calendar.MILLISECOND, 0);
+            DateTime start = new DateTime(cal.getTime());
+            cal.add(java.util.Calendar.DAY_OF_MONTH, +7);
+            DateTime stop = new DateTime(cal.getTime());
+                    
             if (feed.getItems() != null) {
                 for (CalendarListEntry entry : feed.getItems()) {
-                    System.out.println("-----------------------------------------------");
-                    System.out.println("ID: " + entry.getId());
-                    System.out.println("Summary: " + entry.getSummary());
-                    System.out.println("Description: " + entry.getDescription());
+                    System.out.println("User: " + entry.getSummary());
+                    Events events = client.events().list(entry.getId()).setTimeMin(start).setTimeMax(stop).execute();
+        
+                    if(events.getItems() != null)
+                      for (Event event : events.getItems()) {
+                          String a = event.getStart().getDateTime()==null?event.getStart().getDate().toString():event.getStart().getDateTime().toString();
+                          String b = event.getEnd().getDateTime()==null?event.getEnd().getDate().toString():event.getEnd().getDateTime().toString();
+                          System.out.println(event.getSummary() + " - " + a + " - " + b);
+                      }
                 }
             }
                
